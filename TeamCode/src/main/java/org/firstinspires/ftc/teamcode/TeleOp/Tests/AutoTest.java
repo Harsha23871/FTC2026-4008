59c23fabd462fcfa4d2a6b1fd1788d4c62fdf0bd
Harsha23871 <lostharsha23871@gmail.com>
2026-01-03 07:59:55 -0600

    Transfer
    Code transfer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Tests/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Tests/AutoTest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.TeleOp.Tests;//package org.firstinspires.ftc.teamcode.tuning;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.TeleOp.AprilTagsWebCam;
@@ -21,6 +22,7 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+
 import org.firstinspires.ftc.robotcontroller.external.samples.externalhardware.AprilWebcam;
 
 
@@ -32,42 +34,51 @@ public class AutoTest extends LinearOpMode{
 
 
     public class Intake {
-        private DcMotor intake;
+        private DcMotorEx intake;
 
         public Intake(HardwareMap hardwareMap) {
-            intake = hardwareMap.get(DcMotor.class, "Intake");
-            intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            intake.setDirection(DcMotorSimple.Direction.FORWARD);
+            intake = hardwareMap.get(DcMotorEx.class, "intake");
+            intake.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            intake.setDirection(DcMotorEx.Direction.REVERSE);
 
         }
-        public class IntakeIn implements Action{
+        public class IntakeIn implements Action {
             private boolean initialized = false;
+            private long startTime;
+            private final long runTimeMs = 1000; // 1 second (change this)
+
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
                 if (!initialized) {
                     intake.setPower(0.8);
+                    startTime = System.currentTimeMillis();
                     initialized = true;
-                    return true;
-                }
-                else {
-                     intake.setPower(0);
-                        return false;
                 }
 
+                long elapsed = System.currentTimeMillis() - startTime;
+                packet.put("intakeTimeMs", elapsed);
+
+                if (elapsed < runTimeMs) {
+                    return true; // keep running
+                } else {
+                    intake.setPower(0);
+                    return false; // action finished
+                }
             }
         }
+
         public Action intakeIn() {
             return new IntakeIn();
         }
     }
     public class Shooter {
-        private DcMotor outtake;
+        private DcMotorEx outtake;
 
         public Shooter(HardwareMap hardwareMap) {
-            outtake = hardwareMap.get(DcMotor.class, "outtake");
-            outtake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            outtake.setDirection(DcMotor.Direction.FORWARD);
-            outtake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            outtake = hardwareMap.get(DcMotorEx.class, "outtake");
+            outtake.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            outtake.setDirection(DcMotorEx.Direction.FORWARD);
+            outtake.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
         }
         public class ShooterOut implements Action{
@@ -77,6 +88,12 @@ public class AutoTest extends LinearOpMode{
                 if (!initialized) {
                     outtake.setPower(0.8);
                     initialized = true;
+
+                }
+
+                double speedOuttake = outtake.getVelocity();
+                packet.put("outtakeSpeed",speedOuttake);
+                if( speedOuttake < 49){
                     return true;
                 }
                 else {
@@ -149,10 +166,10 @@ public class AutoTest extends LinearOpMode{
     }
 
     public void runOpMode() throws InterruptedException {
-        Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(90));
+        Pose2d initialPose = new Pose2d(-43, 67, Math.toRadians(0));
         MecanumDrive drive = new MecanumDrive(hardwareMap,initialPose);
 
-       // Intake intake = new Intake(hardwareMap);
+       Intake intake = new Intake(hardwareMap);
         Shooter shooter = new Shooter(hardwareMap);
 //        Feeder
 //                feeder = new Feeder(hardwareMap);
@@ -163,10 +180,28 @@ public class AutoTest extends LinearOpMode{
 
 
 
-                TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
-//                        .turn(45)
-                        .lineToYConstantHeading(25)
-                        .waitSeconds(1);
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                .lineToXConstantHeading(40);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//                        .lineToYConstantHeading(25)
 
 
             /*    TrajectoryActionBuilder tab2 = drive.actionBuilder(initialPose)
@@ -178,7 +213,8 @@ public class AutoTest extends LinearOpMode{
                         .lineToYConstantHeading(25)
                         .waitSeconds(1);*/
         Action trajectoryActionCloseOut = tab1.endTrajectory().fresh()
-                .lineToYConstantHeading(0)
+//                .lineToXConstantHeading(7)
+//                .lineToXConstantHeading(60)
                 .build();
 
 
@@ -201,11 +237,13 @@ public class AutoTest extends LinearOpMode{
                  }
                       Actions.runBlocking(
                               new SequentialAction(
-
+                                      //intake.intakeIn(),
 
 
                               trajectoryActionChosen,
-                              trajectoryActionCloseOut,
+                              //shooter.ShootOut(),
+                             // trajectoryActionCloseOut,
+
                                       shooter.ShootOut()
 
 
